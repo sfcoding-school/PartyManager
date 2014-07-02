61268a8eefdd0316ecbc66185deb27d54a64a1d5
Ulisse91 <fedo.coro@gmail.com>
2014-07-02 11:40:32 +0200

    data su EVento, sistemato dialogRispsote

--- app/src/main/java/com/partymanager/helper/HelperDataParser.java
+++ app/src/main/java/com/partymanager/helper/HelperDataParser.java
@@ -2,6 +2,7 @@ package com.partymanager.helper;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
@@ -12,6 +13,8 @@ public class HelperDataParser {
     private static SimpleDateFormat dfgiornoN = new SimpleDateFormat("d", Locale.ITALIAN);
     private static SimpleDateFormat dfgiornoS = new SimpleDateFormat("E", Locale.ITALIAN);
     private static SimpleDateFormat dfMese = new SimpleDateFormat("MMM", Locale.ITALIAN);
+    private static SimpleDateFormat dfMeseN = new SimpleDateFormat("MM", Locale.ITALIAN);
+    private static SimpleDateFormat dfYear = new SimpleDateFormat("yyyy", Locale.ITALIAN);
 
     public static String getMese(GregorianCalendar cal) {
         return dfMese.format(cal.getTime());
@@ -21,8 +24,16 @@ public class HelperDataParser {
         return dfgiornoS.format(cal.getTime());
     }
 
+    public static String getMeseN(GregorianCalendar cal) {
+        return dfMeseN.format(cal.getTime());
+    }
+
     public static String getGiornoNumerio(GregorianCalendar cal) {
         return dfgiornoN.format(cal.getTime());
+    }
+
+    public static String getYear(GregorianCalendar cal) {
+        return dfYear.format(cal.getTime());
     }
 
     public static GregorianCalendar getCalFromString(String data) {
@@ -40,5 +51,19 @@ public class HelperDataParser {
 
     public static String getStringFromCal(GregorianCalendar cal) {
         return cal != null ? dateFormat.format(cal.getTime()) : null;
+    }
+
+    public static long getCountDownDays(int dayMonth, int month, int year) {
+
+        Calendar thatDay = Calendar.getInstance();
+        thatDay.set(Calendar.DAY_OF_MONTH, dayMonth);
+        thatDay.set(Calendar.MONTH, month - 1); // 0-11 so 1 less
+        thatDay.set(Calendar.YEAR, year);
+
+        Calendar today = Calendar.getInstance();
+        long diff = thatDay.getTimeInMillis() - today.getTimeInMillis();
+
+        return diff / (24 * 60 * 60 * 1000);
+
     }
 }
